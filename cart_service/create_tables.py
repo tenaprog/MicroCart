1628a5210d1f06a436a793957af676207e3047d2
tra <tra@entimo.de>
2025-02-10 17:08:07 +0100

    added task-definition file

--- cart_service/create_tables.py
+++ cart_service/create_tables.py
@@ -3,7 +3,7 @@ import boto3
 dynamodb = boto3.resource(
     "dynamodb",
     endpoint_url="http://dynamodb:8000",
-    region_name="us-east-1",
+    region_name="eu-north-1",
     aws_access_key_id="dummy",
     aws_secret_access_key="dummy",
 )
